2c2711c76d395d400ee0a47c589dce7c32b925c1
dalia GUIZ <dalia.guiz@epitech.eu>
2026-03-10 11:18:21 +0100

    feat(front-web):implement home page ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
-import HomeSection from "@/components/Home/home";
+import Home from "@/components/Home/home";
 import Stats from "@/components/Home/home"
 import Navbar from "@/components/navbar";
 import SignupPage from "@/components/signup"
+import HowItWorks from "@/components/Home/HowItWorks"
+import Schools from "@/components/Home/schools"
+import Footer from "@/components/Footer"
+import Quiz from "@/components/Quiz/Quiz";
 
 export default function App() {
   return (
     <main className="min-h-screen bg-white">
-      <Navbar />
-      <Stats />
-      <SignupPage/>
-      <HomeSection />
+      <Navbar/>
+      <Home/>
+      <HowItWorks />
+      <Schools/>
+      <Footer/>
     </main>
   );
 }
